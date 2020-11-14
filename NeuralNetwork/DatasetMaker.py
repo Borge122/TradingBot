d4871c9dce497be8d9a25c312177e7b274840540
Borge122 <gbird1972@hotmail.co.uk>
2020-11-14 22:29:20 +0000

    Update

--- NeuralNetwork/DatasetMaker.py
+++ NeuralNetwork/DatasetMaker.py
@@ -1,6 +1,7 @@
 from subroutines import *
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 """
 This creates a dataset for the neural network to train on. It does require historic data from 20 datasets.
 These datasets must start at the same date and time, and have the same time interval of M20. It must be a chronological csv
@@ -8,13 +9,14 @@ produced by MetaTrader4 or MetaTrader5.
 It is a good idea to include graphs such as US-Oil, FTSE, DAX and DOW-Jones
 If you input AUS2CAD then there is no need to also enter CAD2AUS.
 """
-meta_trader_outputs = [
-    "EURGBP_M20_201501020100_202010302240",
-    "EURUSD_M20_201501020100_202010302240",
-    "GBPUSD_M20_201501020100_202010302240",
-    "USDCHF_M20_201501020100_202010302240",
-    "USDJPY_M20_201501020100_202010302240",
-]
+
+
+meta_trader_outputs = os.listdir(r"C:\Users\Coding Projects\Desktop\Programming Projects\TradingBot\HistoricalRecords")
+meta_trader_outputs = [i.replace(".csv", "") for i in meta_trader_outputs]
+datasets = [load_csv(sets) for sets in meta_trader_outputs]
+print(datasets)
+
+raise
 differential_data = [[(i["CLOSE"]/i["OPEN"])-1 for i in load_csv(set)] for set in meta_trader_outputs]
 for i in range(meta_trader_outputs.__len__()):
     print(differential_data[i].__len__())
